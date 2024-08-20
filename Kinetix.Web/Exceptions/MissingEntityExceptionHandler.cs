5707f0ed0291ad66d44a881fa591b16b1aa5fdb2
Damien <damien.frikha@kleegroup.com>
2024-08-20 16:09:37 +0200

    [Web] Ajout gestion "KeyNotFoundException" dans "MissingEntityExceptionHandler"

--- Kinetix.Web/Exceptions/MissingEntityExceptionHandler.cs
+++ Kinetix.Web/Exceptions/MissingEntityExceptionHandler.cs
@@ -3,7 +3,7 @@
 namespace Kinetix.Web.Exceptions;
 
 /// <summary>
-/// Handler par défaut pour les "Single" en erreur dans EF Core.
+/// Handler par défaut pour les "Single" en erreur dans EF Core et "KeyNotFoundException".
 /// </summary>
 public class MissingEntityExceptionHandler : IKinetixExceptionHandler
 {
@@ -13,11 +13,13 @@ public class MissingEntityExceptionHandler : IKinetixExceptionHandler
     /// <inheritdoc cref="IKinetixExceptionHandler.Handle" />
     public ValueTask<IResult?> Handle(Exception exception)
     {
-        if (exception is not InvalidOperationException { Source: "Microsoft.EntityFrameworkCore" })
+        if (exception is not InvalidOperationException { Source: "Microsoft.EntityFrameworkCore" } and not KeyNotFoundException)
         {
             return ValueTask.FromResult<IResult?>(null);
         }
 
-        return ValueTask.FromResult<IResult?>(Results.NotFound(new KinetixErrorResponse { Errors = ["L'objet demandé n'existe pas."] }));
+        var message = exception is KeyNotFoundException ke && ke.Message != new KeyNotFoundException().Message ? ke.Message : "L'objet demandé n'existe pas.";
+
+        return ValueTask.FromResult<IResult?>(Results.NotFound(new KinetixErrorResponse { Errors = [message] }));
     }
 }
